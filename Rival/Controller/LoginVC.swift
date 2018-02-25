0353fc862c3becf263d5a13f2a087f4d385d5a1d
victorchu8 <victorchu1996@gmail.com>
2018-02-25 11:59:22 -0700

    Keyboard hides when screen tapped outside of textfield

--- Rival/Controller/LoginVC.swift
+++ Rival/Controller/LoginVC.swift
@@ -9,18 +9,25 @@
 import UIKit
 import RevealingSplashView
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController, UITextFieldDelegate {
     
-
+    @IBOutlet weak var emailaddressTxtField: UITextField!
+    @IBOutlet weak var passwordTxtField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Splashview Animation
         let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "rivalIcon")!, iconInitialSize: CGSize(width: 123, height: 115), backgroundColor: #colorLiteral(red: 0.1490196078, green: 0.168627451, blue: 0.1764705882, alpha: 1))
         self.view.addSubview(revealingSplashView)
         revealingSplashView.startAnimation()
-
+        
+        emailaddressTxtField.delegate = self
+        passwordTxtField.delegate = self
+        
+        self.hideKeyboardWhenTappedAround()
     }
+    
+
 
     
 
---
+++ Rival/Extensions/TextFieldTapped.swift
@@ -0,0 +1,27 @@
+//
+//  DimissKeyboardWhenTapped.swift
+//  Rival
+//
+//  Created by VICTOR CHU on 2018-02-25.
+//  Copyright © 2018 Victor Chu. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func hideKeyboardWhenReturnTapped(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
