b852d255c36e2e0ba4585535d5032f81b3457e3d
victorchu8 <victorchu1996@gmail.com>
2018-03-08 11:21:48 -0700

    minor calendar code added

--- Rival/Controller/GroupsVC.swift
+++ Rival/Controller/GroupsVC.swift
@@ -17,6 +17,8 @@ protocol GroupsVCDelegate: class {
 
 class GroupsVC: UIViewController, SideMenuVCDelegate {
     
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
     private var sideMenuVCNavigationController: UISideMenuNavigationController?
     weak var delegate: GroupsVCDelegate?
     
@@ -26,6 +28,7 @@ class GroupsVC: UIViewController, SideMenuVCDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        calendarView.scrollToDate(Date())
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -55,22 +58,28 @@ extension GroupsVC: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
         formatter.timeZone = Calendar.current.timeZone
         formatter.locale = Calendar.current.locale
         
-        let startDate = formatter.date(from: "2017 01 01")
-        let endDate = formatter.date(from: "2017 12 31")
-        let numberOfRows = 1
-        
-        let parameters = ConfigurationParameters(startDate: startDate!, endDate: endDate!, numberOfRows: numberOfRows)
+        var dateComponent = DateComponents()
+        dateComponent.year = 1
+        let startDate = Date()
+        let endDate = Calendar.current.date(byAdding: dateComponent, to: startDate)
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate!, numberOfRows: 1, calendar: Calendar.current, generateInDates: .forFirstMonthOnly, generateOutDates: .off, firstDayOfWeek: .sunday, hasStrictBoundaries: false)
         return parameters
     }
     
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
-        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
-        self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
-        cell.dateLabel.text = cellState.text
-        return cell
+        let calendarCell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
+        sharedFunctionToConfigureCell(calendarCell: calendarCell, cellState: cellState, date: date)
+        return calendarCell
     }
     
     func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let calendarCell = cell as! CalendarCell
+        sharedFunctionToConfigureCell(calendarCell: calendarCell, cellState: cellState, date: date)
+        
+    }
+    
+    func sharedFunctionToConfigureCell(calendarCell: CalendarCell, cellState: CellState, date: Date) {
+        calendarCell.dateLabel.text = cellState.text
     }
     
 
